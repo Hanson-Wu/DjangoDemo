f660896562c6969f955d1ef47c0a69f1326a9363
Hanson-Wu <hansonwu@outlook.com>
2016-09-18 15:36:25 -0700

    Add favorite form/css/imags

--- website/music/views.py
+++ website/music/views.py
@@ -1,36 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Album
-from django.shortcuts import render
+from .models import Album, Song
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 # Create your views here.
 
 def index(request):
 	all_albums = Album.objects.all()
-	template = loader.get_template('music/index.html')
-	# html = ''
-	# for album in all_albums:
-	# 	url = '/music/' + str(album.id) + '/'
-	# 	html += '<a href = "'+ url +'">' + album.album_title + '</a><br>'
-	# return HttpResponse(html)
 
 	context  = {
 		'all_albums':all_albums,
 	}
-	# return HttpResponse(template.render(context, request))
-
 	return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-	#return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+	album = get_object_or_404(Album, pk = album_id)
+	return render(request, 'music/detail.html', {'album':album})
 
+def favorite(request, album_id):
+	album = get_object_or_404(Album, pk=album_id)
 	try:
-		album = Album.objects.get(pk = album_id)
-		context = {
+		selected_song = album.song_set.get(pk = request.POST['song'])
+	except (KeyError, Song.DoesNotExist):
+		return render(request, 'music/detail.html', {
 			'album': album,
-		}
-	except Album.DoesNotExist:
-		raise Http404("Album does not exits")
-	return render(request, 'music/detail.html', context)
-
+			'error_message': 'You did not select a valid song',
+		})
+	else:
+		selected_song.is_favorite = True
+		selected_song.save()
+		return render(request, 'music/detail.html', {'album': album})
